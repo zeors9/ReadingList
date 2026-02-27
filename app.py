a08f7a2ade3c7fbe2763b671ad467cf9bd7d9e27
zeors9 <zeors9@users.noreply.github.com>
2026-02-27 18:35:40 -0500

    Added Fun tag handling, additional entries on list

--- app.py
+++ app.py
@@ -13,11 +13,13 @@ with st.sidebar:
     media_filter = st.multiselect("Media Type", options=dataList['Media'].unique())
     tag_filter = st.multiselect("Tags", options=set(tag for tags in dataList['Tags'] for tag in tags))
     author_filter = st.multiselect("Authors", options=set(author for authors in dataList['Authors'] for author in authors))
+    fun_filter = st.checkbox("Fun", value=False)
 
 filteredData = dataList[
     (dataList['Media'].isin(media_filter) if media_filter else pd.Series(True, index=dataList.index)) &
     (dataList['Tags'].apply(lambda tags: any(tag in tags for tag in tag_filter)) if tag_filter else pd.Series(True, index=dataList.index)) &
-    (dataList['Authors'].apply(lambda authors: any(author in authors for author in author_filter)) if author_filter else pd.Series(True, index=dataList.index))
+    (dataList['Authors'].apply(lambda authors: any(author in authors for author in author_filter)) if author_filter else pd.Series(True, index=dataList.index)) &
+    (~dataList['Tags'].apply(lambda tags: "fun" in tags) if not fun_filter else pd.Series(True, index=dataList.index))
 ]
 
 
